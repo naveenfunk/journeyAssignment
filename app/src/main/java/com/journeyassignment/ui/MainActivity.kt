e716b6a096554ae811986d60aba63e4b3ac8efa2
Naveen <naveen11233@gmail.com>
2022-08-07 00:18:04 +0530

    Project setup - UI ready

---
+++ app/src/main/java/com/journeyassignment/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.journeyassignment.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.journeyassignment.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/journeyassignment/ui/post_list/PostListFragment.kt
@@ -0,0 +1,20 @@
+package com.journeyassignment.ui.post_list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.journeyassignment.R
+
+class PostListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_post_list, container, false)
+    }
+
+}
